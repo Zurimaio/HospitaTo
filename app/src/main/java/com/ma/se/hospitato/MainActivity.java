f159d451c5175668a85f715c0a62335aedfd53e2
Zurimaio <maufloridia@gmail.com>
2018-11-24 12:55:52 +0100

    Map View added; Double language file added

--- app/src/main/java/com/ma/se/hospitato/MainActivity.java
+++ app/src/main/java/com/ma/se/hospitato/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ma.se.hospitato;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        //Used temporary to go directly to the map view
+        Button mapView = findViewById(R.id.toMapView);
+        toMapViewButton(mapView);
+
+    }
+
+    //Temporary to go to the map view
+    public void toMapViewButton(Button mapView){
+        mapView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMapView = new Intent(MainActivity.this, MapView.class);
+                startActivity(toMapView);
+            }
+        });
     }
 }
