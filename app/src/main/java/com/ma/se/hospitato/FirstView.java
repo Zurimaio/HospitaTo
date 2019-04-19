00242dd6f8d82dbe57734b7d95b2150b58cf93a9
StefaniaTitone <stefaniatitone1993@gmail.com>
2019-04-19 19:57:11 +0200

    FACEBOOK LOGIN

--- app/src/main/java/com/ma/se/hospitato/FirstView.java
+++ app/src/main/java/com/ma/se/hospitato/FirstView.java
@@ -1,31 +1,158 @@
 package com.ma.se.hospitato;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.BreakIterator;
 
 public class FirstView extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     private Intent intent;
+    FirebaseDatabase mDatabase;
+    private CallbackManager mCallbackManager;
+    private static final String TAG= "FACELOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_view);
+
+        //FOR THE HASH KEY OF THE PHONE
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo( "com.ma.se.hospitato", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures)
+            { MessageDigest md = MessageDigest.getInstance("SHA");
+            md.update(signature.toByteArray());
+            Log.d("KeyHash", "KeyHash:" + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            //Toast.makeText(getApplicationContext(), Base64.encodeToString(md.digest(), Base64.DEFAULT), Toast.LENGTH_LONG).show();
+            }
+        } catch (PackageManager.NameNotFoundException e)
+        {
+
+        } catch (NoSuchAlgorithmException e) { }
+
         mAuth = FirebaseAuth.getInstance();
-        if(mAuth.getCurrentUser() != null){
+        mDatabase=FirebaseDatabase.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
             intent = new Intent(this, Main2Activity.class);
             startActivity(intent);
             finish();
         }
 
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser !=null){
+            updateUI(currentUser);
+        }
+
+    }
+
+    @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Pass the activity result back to the Facebook SDK
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            DatabaseReference ref = mDatabase.getReference("Users/" + user.getUid());
+                            Profile ru = new Profile();
+                            ru.setName(user.getDisplayName());
+                            ru.setEmail(user.getEmail());
+                            ru.setSurname("-");
+                            ru.setNascita("gg/mm/aaaa");
+                            ru.setWeight(null);
+                            ru.setHeight(null);
+                            ru.setBlood(null);
+                            ref.setValue(ru);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(FirstView.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 
 
 
@@ -47,6 +174,14 @@ public class FirstView extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    private void updateUI(FirebaseUser user) {
+        intent=new Intent(this,Main2Activity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 
 
 
