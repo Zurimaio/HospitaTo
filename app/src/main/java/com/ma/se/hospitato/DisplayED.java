a6895350b0d08a3711a5983978bd01456d4d0b8e
Zurimaio <maufloridia@gmail.com>
2019-04-27 19:42:29 +0200

    Integration of AI

--- app/src/main/java/com/ma/se/hospitato/DisplayED.java
+++ app/src/main/java/com/ma/se/hospitato/DisplayED.java
@@ -1,6 +1,8 @@
 package com.ma.se.hospitato;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.common.util.ArrayUtils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
@@ -16,9 +19,27 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.common.modeldownload.FirebaseRemoteModel;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
 
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.logging.StreamHandler;
 
@@ -76,6 +97,7 @@ public class DisplayED extends Fragment implements OnMapReadyCallback {
         travTime = getArguments().getString("TravelTime");
 
         Log.d("FILTER TRAVEL TIME", travTime);
+        //ML();
 
     }
 
@@ -124,7 +146,6 @@ public class DisplayED extends Fragment implements OnMapReadyCallback {
         return view;
     }
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
@@ -149,7 +170,6 @@ public class DisplayED extends Fragment implements OnMapReadyCallback {
         this.loc = loc;
     }
 
-
     private void setPeopleInPS() {
 
         new Thread() {
@@ -165,7 +185,10 @@ public class DisplayED extends Fragment implements OnMapReadyCallback {
                             }
                             waiting = result.get("waiting");
                             treatment = result.get("treatment");
-                            System.out.println(waiting);
+                            System.out.println("Waiting " + waiting.toString());
+                            System.out.println("Treatment " + treatment.toString());
+
+                            ML();
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -212,5 +235,105 @@ public class DisplayED extends Fragment implements OnMapReadyCallback {
         }.start();
 
     }
+
+
+
+
+
+    public void ML(){
+        FirebaseLocalModel localSource =
+                new FirebaseLocalModel.Builder("molinette")  // Assign a name to this model
+                        .setAssetFilePath("molinette.tflite")
+                        .build();
+        FirebaseModelManager.getInstance().registerLocalModel(localSource);
+
+        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
+                .setLocalModelName("molinette")
+                .build();
+        try {
+            FirebaseModelInterpreter firebaseInterpreter =
+                    FirebaseModelInterpreter.getInstance(options);
+
+            FirebaseModelInputOutputOptions inputOutputOptions =
+                    new FirebaseModelInputOutputOptions.Builder()
+                            .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 9})
+                            .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 1})
+                            .build();
+
+
+
+        float[][] input = toIntegerArrayFromHashMap(waiting, treatment);
+
+
+        //query to the model
+        FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
+                .add(input)  // add() as many input arrays as your model requires
+                .build();
+        firebaseInterpreter.run(inputs, inputOutputOptions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseModelOutputs>() {
+                            @Override
+                            public void onSuccess(FirebaseModelOutputs result) {
+
+                               float[][] output = result.getOutput(0);
+                               System.out.println("Output");
+                               float out = output[0][0];
+                               System.out.println(out);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                                System.out.println("SCAAAAAAAAAAAASSSSOOO");
+                                e.printStackTrace();
+                                e.getCause();
+                            }
+                        });
+
+        }catch (FirebaseMLException e){
+            Log.e("ERROR", "InputOutput");
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+    public float[][] toIntegerArrayFromHashMap(HashMap<String, String> waiting, HashMap<String, String> treatment){
+        String[] wait = waiting.values().toArray(new String[4]);
+        String[] treat = treatment.values().toArray(new String[4]);
+        float[][] array =new float[1][wait.length+treat.length+1];
+
+        for(int i = 0; i < array.length; i++) {
+            for (String str : wait)
+                array[0][i++] = Float.parseFloat(str);
+
+            for (String str : treat)
+                array[0][i++] = Float.parseFloat(str);
+        }
+         /*
+        Python          Java
+        Lunedì      0 - 2
+        Martedì     1 - 3
+        Mercoledì   2 - 4
+        Giovedì     3 - 5
+        Venerdì     4 - 6
+        Sabato      5 - 7
+        Domenica    6 - 1
+         */
+
+        float day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        //transforming day of week from java to python
+        day = day - 2;
+        array[0][8] = day;
+        return array;
+
+
+    }
+
+
 
 }
